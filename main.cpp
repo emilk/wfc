955bd4c54cca319dc8451051fb19dc19e73b5de6
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2016-10-02 22:25:22 +0200

    Add check for too large palette and/or N

--- main.cpp
+++ main.cpp
@@ -270,6 +270,10 @@ size_t spin_the_bottle(const std::vector<double>& a, double between_zero_and_one
 
 PatternHash hash_from_pattern(const Pattern& pattern, size_t palette_size)
 {
+	CHECK_LT_F(std::pow((double)palette_size, (double)pattern.size()),
+	           std::pow(2.0, sizeof(PatternHash) * 8),
+	           "Too large palette (it is %lu) or too large pattern size (it's %.0f)",
+	           palette_size, std::sqrt(pattern.size()));
 	PatternHash result = 0;
 	size_t power = 1;
 	for (const auto i : irange(pattern.size()))
