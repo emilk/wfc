ac694d87c22abfb4d4c960854829f1e08363de5a
Jett <whackashoe@gmail.com>
2016-10-21 23:26:08 -0500

    move model to new file

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@
 #include "result.hpp"
 #include "paletted_image.hpp"
 #include "output.hpp"
+#include "model.hpp"
 
 
 const char* result2str(const Result result)
@@ -54,23 +55,6 @@ Image upsample(const Image& image)
 }
 
 // ----------------------------------------------------------------------------
-
-class Model
-{
-public:
-	size_t              _width;      // Of output image.
-	size_t              _height;     // Of output image.
-	size_t              _num_patterns;
-	bool                _periodic_out;
-	size_t              _foundation = kInvalidIndex; // Index of pattern which is at the base, or kInvalidIndex
-
-	// The weight of each pattern (e.g. how often that pattern occurs in the sample image).
-	std::vector<double> _pattern_weight; // num_patterns
-
-	virtual bool propagate(Output* output) const = 0;
-	virtual bool on_boundary(int x, int y) const = 0;
-	virtual Image image(const Output& output) const = 0;
-};
 
 // ----------------------------------------------------------------------------
 
